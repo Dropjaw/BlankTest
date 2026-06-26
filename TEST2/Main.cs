bd952465a93b95a2b0268c9ab4f86f795aa781a4
Dropjaw <games@spacemarines.co.uk>
2026-06-26 08:24:55 +0100

    Create TEST2 entry point

---
+++ TEST2/Main.cs
@@ -0,0 +1,10 @@
+namespace TEST2
+{
+    internal static class MainEntry
+    {
+        private static void Main(string[] args)
+        {
+            System.Console.WriteLine(App.GetMessage());
+        }
+    }
+}
